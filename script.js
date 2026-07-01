bf1512d19e55270f92d2d27d99e24acc5478da78
.🦋 ~idza~🦋 <idzakalu3@gmail.com>
2026-07-01 23:29:10 +0300

    sign up and log in

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ function login(){
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     let message = document.getElementById("message");
-    if(username === "admin" && password === "password123"){
+
+    let savedUsername = localStorage.getItem("username");
+    let savedPassword = localStorage.getItem("password");
+
+    if(username === savedUsername && password === savedPassword){
         message.style.color = "green";
-        message.textcontent = "Login successful!";
+        message.textContent = "Login successful!";
         //redirect after two seconds
         setTimeout(function(){
             window.location.href = "home.html";
@@ -13,5 +17,25 @@ function login(){
         message.style.color = "red";
         message.textContent = "Invalid username or password!";
     }
+
     
+   function signup() {
+    let fullname = document.getElementById("fullname").value;
+    let email = document.getElementById("email").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let message = document.getElementById("message");
+
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    localStorage.setItem("fullname", fullname);
+    localStorage.setItem("email", email);
+
+    message.style.color = "green";
+    message.textContent = "Account created successfully!";
+
+    setTimeout(function () {
+        window.location.href = "login.html";
+    }, 2000);
+}
 }
